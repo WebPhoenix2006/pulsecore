9533113ea6a58da235f00dd5236ec471ba299272
Samuel Emeka <devsamuel0611@gmail.com>
2025-09-15 21:02:35 +0100

    added layout for catalog

--- admin-portal/src/app/catalog/catalog-module.ts
+++ admin-portal/src/app/catalog/catalog-module.ts
@@ -4,16 +4,11 @@ import { CommonModule } from '@angular/common';
 import { CatalogRoutingModule } from './catalog-routing-module';
 import { Category } from './components/category/category';
 import { Products } from './components/products/products';
-
+import { SharedModule } from '../shared/shared-module';
+import { Layout } from './components/layout/layout';
 
 @NgModule({
-  declarations: [
-    Category,
-    Products
-  ],
-  imports: [
-    CommonModule,
-    CatalogRoutingModule
-  ]
+  declarations: [Category, Products, Layout],
+  imports: [CommonModule, CatalogRoutingModule, SharedModule],
 })
-export class CatalogModule { }
+export class CatalogModule {}
--- admin-portal/src/app/shared/components/sidebar/sidebar.ts
+++ admin-portal/src/app/shared/components/sidebar/sidebar.ts
@@ -1,20 +1,11 @@
-import { Component, input, output } from '@angular/core';
-import { SidebarRoute } from '../../../interfaces/sidebar-route.interface';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   standalone: false,
   templateUrl: './sidebar.html',
-  styleUrl: './sidebar.scss',
+  styleUrl: './sidebar.scss'
 })
 export class Sidebar {
-  isLeftSidebarCollapsed = input.required<boolean>();
-  links = input<SidebarRoute[]>();
-  toggleState = output<boolean>();
 
-  items = [
-    { routeLink: '/', icon: 'fa fa-home', label: 'Home' },
-    { routeLink: '/pages', icon: 'fa fa-file', label: 'Pages' },
-    { routeLink: '/products', icon: 'fa fa-box-open', label: 'Products' },
-  ];
 }
